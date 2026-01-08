8dc6f37a4b2b12acc31ab9e9f80483ceae410a63
Aditya Raj Gupta <aditya9199034744@gmail.com>
2026-01-08 09:50:29 +0530

    practice set

---
+++ practice2.js
@@ -0,0 +1,12 @@
+//write a code to add a number in front of array
+
+//arr = [2,20,40,5]
+// number = 10
+// output = [10,2,20,40,5]
+
+let arr = [2,20,40,5];
+num = 10; 
+
+arr.unshift(num);
+
+console.log(arr);
